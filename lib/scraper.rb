728255bd0747e6cf01ac3be6f39e7664b8ae72ed
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-15 12:18:17 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,10 @@ html = open("https://flatironschool.com/") #stores html in a temporary file
 
 #HTML is a method of the gem Nokogiri and converts the large string into a bunch of nested nodes
 doc = Nokogiri::HTML(html)
+<<<<<<< HEAD
 
 
 doc.css(".headline-26OIBN").text
 puts doc.css(".headline-26OIBN").text
+=======
+>>>>>>> 571614cbd340b4e61c13e6a4afb5b48fd90e3ee8
